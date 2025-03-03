4979adb0eddd0ba9d0d0c42539b53e23259b6db3
Sudhanshu singh <ss826063@gmail.com>
2025-03-03 23:30:03 +0530

    admin panel start

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,26 @@
-import { useState } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./pages/main/Main";
 import Lesson from "./pages/main/Lesson";
-import { ObjectiveType } from "./components/Quizes/ObjectiveType";
-import DragDropSortGame from "./components/Quizes/DragAndDrop";
 import Admin from "./pages/admin/Admin";
+import Test from "./pages/admin/Test";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 {/* <Route path="/" element={} /> */}
-                <Route path="/admin" element={<Admin />} />
+                <Route path="/admin" element={<Admin />}>
+                    <Route path="test" element={<div>lang</div>} />
+                    <Route path="courses" element={<div>courses</div>} />
+                    <Route
+                        path="/admin/chapters"
+                        element={<div>chapters</div>}
+                    />
+                </Route>
                 <Route path="/lesson" element={<Main />} />
 
                 <Route path="/lessonQuiz/:lessonId" element={<Lesson />} />
-
-                {/* todo later render these through only multistepper from */}
-                <Route path="/objective" element={<ObjectiveType />} />
-                <Route path="/drag" element={<DragDropSortGame />} />
             </Routes>
         </BrowserRouter>
     );
--- frontend/src/pages/admin/Admin.jsx
+++ frontend/src/pages/admin/Admin.jsx
@@ -1,7 +1,19 @@
-export default function Admin(){
+import "./admin.css";
+import { Route, Routes, Link, BrowserRouter, Outlet } from "react-router-dom";
+import Test from "./Test";
+export default function Admin() {
+    return (
+        <div className="grid">
+            <div className="sidebar">
+                <Link to="/admin/test">Language</Link>
+                <Link to="/admin/courses">Courses</Link>
+                <div>Chapters</div>
+                <div>Quizes</div>
+            </div>
+            <div className="main-content">
+                <Outlet />
 
-    return <div>
-        <div>sidebar</div>
-        <div>main content</div>
-    </div>
+            </div>
+        </div>
+    );
 }
