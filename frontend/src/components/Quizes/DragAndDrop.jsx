8f3d8a5532ab05aa67fd04150d48c379ee988361
Sudhanshu singh <ss826063@gmail.com>
2025-03-19 01:55:14 +0530

    finally a woking solution for dynamic component

--- frontend/src/components/Quizes/DragAndDrop.jsx
+++ frontend/src/components/Quizes/DragAndDrop.jsx
@@ -10,26 +10,29 @@ export default function DragDropSortGame({ data }) {
         setDragWords(data?.words);
     }, [data]);
     return (
-        <>
-            <div>hehehe</div>
-            <div>
-                {dragCategoriesWords?.map((element, index) => {
-                    return (
-                        <p className="flex gap-2 w-20 aspect-square bg-amber-600 text-white">
-                            {element}
-                        </p>
-                    );
-                })}
-            </div>
-            <div>
+        <div className="max-w-[600px]">
+            <div>Go ahead put 'em where they belong</div>
+            <div className="border flex gap-2">
                 {dragWords?.map((element, index) => {
                     return (
-                        <p className="w-20 aspect-square bg-amber-600 text-white">
+                        <p
+                            draggable
+                            className="w-20 aspect-video flex justify-center items-center bg-green-500 rounded-lg text-white"
+                        >
                             {element?.text}
                         </p>
                     );
                 })}
             </div>
-        </>
+            <div className="border flex gap-2 mt-4 justify-evenly items-center">
+                {dragCategoriesWords?.map((element, index) => {
+                    return (
+                        <p className="flex gap-2 w-40 aspect-square bg-emerald-500 text-white">
+                            {element}
+                        </p>
+                    );
+                })}
+            </div>
+        </div>
     );
 }
